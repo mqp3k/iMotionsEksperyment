4a8eb07d1201791716cae0ce4d3ed3834812e9e1
mqp3k <246729@student.pwr.edu.pl>
2024-04-10 00:07:28 +0200

    Java likes tests update

--- ZadaniaJava/ZadaniaJava/UnitTesting/WhoLikesItTests.java
+++ ZadaniaJava/ZadaniaJava/UnitTesting/WhoLikesItTests.java
@@ -11,30 +11,30 @@ public class WhoLikesItTests {
     @Test
     public void testEmptyArray() {
         String[] people = {};
-        assertEquals("no one likes this", Main.whoLikesIt(people));
+        assertEquals("nikt tego nie lubi", Main.whoLikesIt(people));
     }
 
     @Test
     public void testOnePerson() {
         String[] people = { "Peter" };
-        assertEquals("Peter likes this", Main.whoLikesIt(people));
+        assertEquals("Peter lubi to", Main.whoLikesIt(people));
     }
 
     @Test
     public void testTwoPeople() {
         String[] people = { "Jacob", "Alex" };
-        assertEquals("Jacob and Alex like this", Main.whoLikesIt(people));
+        assertEquals("Jacob i Alex lubią to", Main.whoLikesIt(people));
     }
 
     @Test
     public void testThreePeople() {
         String[] people = { "Max", "John", "Mark" };
-        assertEquals("Max, John and Mark like this", Main.whoLikesIt(people));
+        assertEquals("Max, John i Mark lubią to", Main.whoLikesIt(people));
     }
 
     @Test
     public void testMoreThanThreePeople() {
         String[] people = { "Alex", "Jacob", "Mark", "Max" };
-        assertEquals("Alex, Jacob and 2 others like this", Main.whoLikesIt(people));
+        assertEquals("Alex, Jacob i 2 inne osoby lubią to", Main.whoLikesIt(people));
     }
 }
