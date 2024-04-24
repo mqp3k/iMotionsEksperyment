8a0cf7bfb3fd3496c349a9b12d419bce0bd81e68
m-suwala <ancymoncia@gmail.com>
2024-04-24 16:26:43 +0200

    fix test naming

--- ZadaniaPython/UnitTesting/sum_multiples_test.py
+++ ZadaniaPython/UnitTesting/sum_multiples_test.py
@@ -3,22 +3,22 @@ from Zadania.main import sumMultiples
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_solution_example_1(self):
+    def test_solution_limit_10(self):
         self.assertEqual(23, sumMultiples(10))
 
-    def test_solution_example_2(self):
+    def test_solution_limit_3(self):
         self.assertEqual(0, sumMultiples(3))
 
-    def test_solution_example_3(self):
+    def test_solution_limit_1(self):
         self.assertEqual(0, sumMultiples(1))
 
-    def test_solution_example_4(self):
+    def test_solution_limit_0(self):
         self.assertEqual(0, sumMultiples(0))
 
-    def test_solution_example_5(self):
+    def test_solution_limit_20(self):
         self.assertEqual(78, sumMultiples(20))
 
-    def test_solution_example_6(self):
+    def test_solution_limit_1000(self):
         self.assertEqual(233168, sumMultiples(1000))
 
 if __name__ == '__main__':
