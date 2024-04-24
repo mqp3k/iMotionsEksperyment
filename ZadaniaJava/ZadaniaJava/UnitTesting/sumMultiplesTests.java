f5552bfda579b37f3946f544656c8ef6466ff8ee
m-suwala <ancymoncia@gmail.com>
2024-04-24 16:18:14 +0200

    fix test naming

--- ZadaniaJava/ZadaniaJava/UnitTesting/sumMultiplesTests.java
+++ ZadaniaJava/ZadaniaJava/UnitTesting/sumMultiplesTests.java
@@ -8,32 +8,32 @@ import ZadaniaJava.Zadania.Main;
 
 public class sumMultiplesTests {
     @Test
-    public void testSolutionExample1() {
+    public void testSolutionLimit10() {
         assertEquals(23, Main.sumMultiples(10));
     }
 
     @Test
-    public void testSolutionExample2() {
+    public void testSolutionLimit3() {
         assertEquals(0, Main.sumMultiples(3));
     }
 
     @Test
-    public void testSolutionExample3() {
+    public void testSolutionLimit1() {
         assertEquals(0, Main.sumMultiples(1));
     }
 
     @Test
-    public void testSolutionExample4() {
+    public void testSolutionLimit0() {
         assertEquals(0, Main.sumMultiples(0));
     }
 
     @Test
-    public void testSolutionExample5() {
+    public void testSolutionLimit20() {
         assertEquals(78, Main.sumMultiples(20));
     }
 
     @Test
-    public void testSolutionExample6() {
+    public void testSolutionLimit1000() {
         assertEquals(233168, Main.sumMultiples(1000));
     }
 }
